b4d6eeb85e680de81ac7aa00bbd6b4135410c2e4
SHISHIRA <Shishiras98@gmail.com>
2021-08-18 14:43:47 +0530

    Added: Calculated daily wage using switch case

--- src/com/bridgelabz/employeewage/EmployeeWageBuilder.java
+++ src/com/bridgelabz/employeewage/EmployeeWageBuilder.java
@@ -1,20 +1,42 @@
 package com.bridgelabz.employeewage;
 
+import java.util.Random;
+
 public class EmployeeWageBuilder {
 
 	public static void main(String[] args) {
-		int isPresent = 1;
 		int wagePerHr = 20;
 		int fullDayHr = 8;
-		int finalWage;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == isPresent) {
-			finalWage = (wagePerHr * fullDayHr);
-			System.out.println("Employee is Full Time Present!");
-			System.out.println("Daily wage of an Full timer employee is" + " " + finalWage + " Rs");
-		} else {
-			System.out.println("Employee is Absent!");
+		int partDayHr = 4;
+		int dayInMonth = 20;
+		int dayWage;
+		int monthlyWage;
 
+		Random rmd = new Random();
+		int empCheck = rmd.nextInt(3);
+		switch (empCheck) {
+		case 2: {
+			dayWage = (wagePerHr * partDayHr);
+			monthlyWage = (dayInMonth * dayWage);
+			System.out.println("Employee is part time present!");
+			System.out.println("Daily wage of an part timer employee is" + " " + dayWage + " " + "Rs");
+			System.out.println("Monthly wage of an part timer employee is" + " " + monthlyWage + " " + "Rs");
+			break;
+		}
+		case 1: {
+			dayWage = (wagePerHr * fullDayHr);
+			monthlyWage = (dayInMonth * dayWage);
+			System.out.println("Employee is Full Time Present!");
+			System.out.println("Daily wage of an full timer employee is" + " " + dayWage + " " + "Rs");
+			System.out.println("Monthly wage of an full timer employee is" + " " + monthlyWage + " " + "Rs");
+			break;
+		}
+		case 0: {
+			System.out.println("Employee is Absent!");
+			break;
+		}
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + empCheck);
 		}
 	}
 }
