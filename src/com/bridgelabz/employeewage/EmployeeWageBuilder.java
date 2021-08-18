ec315c3683ad5872ac3ccc851e10f4aa7a9d1e52
SHISHIRA <Shishiras98@gmail.com>
2021-08-18 12:00:47 +0530

    Added: Calculated daily wage of employee

--- src/com/bridgelabz/employeewage/EmployeeWageBuilder.java
+++ src/com/bridgelabz/employeewage/EmployeeWageBuilder.java
@@ -4,10 +4,17 @@ public class EmployeeWageBuilder {
 
 	public static void main(String[] args) {
 		int isPresent = 1;
+		int wagePerHr = 20;
+		int fullDayHr = 8;
+		int finalWage;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == isPresent)
+		if (empCheck == isPresent) {
+			finalWage = (wagePerHr * fullDayHr);
 			System.out.println("Employee is Full Time Present!");
-		else
+			System.out.println("Daily wage of an Full timer employee is" + " " + finalWage + " Rs");
+		} else {
 			System.out.println("Employee is Absent!");
+
+		}
 	}
 }
