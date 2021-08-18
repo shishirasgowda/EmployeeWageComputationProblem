7ec67a9c074f01e5a174252d3588cf5c04b95a06
SHISHIRA <Shishiras98@gmail.com>
2021-08-18 11:45:19 +0530

    Added: checked employee present or absent

--- src/com/bridgelabz/employeewage/EmployeeWageBuilder.java
+++ src/com/bridgelabz/employeewage/EmployeeWageBuilder.java
@@ -3,6 +3,11 @@ package com.bridgelabz.employeewage;
 public class EmployeeWageBuilder {
 
 	public static void main(String[] args) {
-		System.out.println("welcome to employee wage computation problem");
+		int isPresent = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == isPresent)
+			System.out.println("Employee is Full Time Present!");
+		else
+			System.out.println("Employee is Absent!");
 	}
 }
