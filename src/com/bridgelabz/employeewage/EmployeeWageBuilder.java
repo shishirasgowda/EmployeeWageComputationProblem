82adfcc537fc6b6d2c0675542d196c41d4108b81
SHISHIRA <Shishiras98@gmail.com>
2021-08-18 11:35:03 +0530

    Added

---
+++ src/com/bridgelabz/employeewage/EmployeeWageBuilder.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.employeewage;
+
+public class EmployeeWageBuilder {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to employee wage computation problem");
+	}
+}
